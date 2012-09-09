db875201d6be0d4dfbd534a2a1b152b76d57849b
Jesse Jones <jesse9jones@gmail.com>
2012-09-09 13:23:13 -0700

    Simplified lib exports

--- src/rwebserve.rs
+++ src/rwebserve.rs
@@ -1,29 +1,31 @@
 //! Public API for rwebserve.
-
-// TODO: Hopefully we can clean up the configuration exporting when rust works a bit better.
+use configuration::*;
+use imap::*;
+use server::*;
+use sse::*;
 
 // configuration
-use Config = configuration::Config; export Config;
-use Request = configuration::Request; export Request;
-use Response = configuration::Response; export Response;
-use ResponseHandler = configuration::ResponseHandler; export ResponseHandler;
-use RsrcLoader = configuration::RsrcLoader; export RsrcLoader;
-use RsrcExists = configuration::RsrcExists; export RsrcExists;
-use Route = configuration::Route; export Route;
-use initialize_config = configuration::initialize_config; export initialize_config;
+export Config;
+export Request;
+export Response;
+export ResponseHandler;
+export RsrcLoader;
+export RsrcExists;
+export Route;
+export initialize_config;
 
 // imap
-use IMap = imap::IMap; export IMap;
-use ImmutableMap = imap::ImmutableMap; export ImmutableMap;
+export IMap;
+export ImmutableMap;
 
 // server
-use start = server::start; export start;
+export start;
 
 // sse
-use OpenSse = sse::OpenSse; export OpenSse;
-use PushChan = sse::PushChan; export PushChan;
-use ControlPort = sse::ControlPort; export ControlPort;
-use ControlChan = sse::ControlChan; export ControlChan;
-use ControlEvent = sse::ControlEvent; export ControlEvent;
-use RefreshEvent = sse::RefreshEvent; export RefreshEvent;
-use CloseEvent = sse::CloseEvent; export CloseEvent;
+export OpenSse;
+export PushChan;
+export ControlPort;
+export ControlChan;
+export ControlEvent;
+export RefreshEvent;
+export CloseEvent;
